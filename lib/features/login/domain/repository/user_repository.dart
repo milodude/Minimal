731df9524dca864a981f7526ca0085efe2251075
Matias Raverta <mraverta@nuba.com.ar>
2022-06-14 14:17:17 -0300

    3-Implementation of Domain user repository

--- lib/features/login/domain/repository/user_repository.dart
+++ lib/features/login/domain/repository/user_repository.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
 
-import 'package:coda_test/features/login/domain/entities/user_info.dart';
+import '../../../../core/error/failure.dart';
+import '../entities/user_info.dart';
 
 abstract class  UserRepository{
-  //Future<Either<Failure, UserInfo>> login();
+  Future<Either<Failure, UserInfo>> login({required String userName, required String password});
 }

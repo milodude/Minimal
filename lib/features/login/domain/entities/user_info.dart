c5db83776add30c821d2526e478574d06b7bc45c
Matias Raverta <mraverta@nuba.com.ar>
2022-06-14 11:30:21 -0300

    1 - Initial commit Coda Test

---
+++ lib/features/login/domain/entities/user_info.dart
@@ -0,0 +1,17 @@
+class UserInfo {
+  int id;
+  String firstName;
+  String lastName;
+  String email;
+  String photo;
+  String accessToken;
+  
+  UserInfo({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.photo,
+    required this.accessToken,
+  });
+}
---
+++ lib/features/login/domain/repository/user_repository.dart
@@ -0,0 +1,6 @@
+
+import 'package:coda_test/features/login/domain/entities/user_info.dart';
+
+abstract class  UserRepository{
+  //Future<Either<Failure, UserInfo>> login();
+}

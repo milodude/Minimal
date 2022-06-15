0d37b480c0917237036bb13497dc9cad780e6d52
Matias Raverta <mraverta@nuba.com.ar>
2022-06-15 17:48:09 -0300

    13-Added base login in order to control redirection after loggging in

--- lib/main.dart
+++ lib/main.dart
@@ -18,15 +18,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<UserBloc>(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData().copyWith(textTheme: GoogleFonts.dmSansTextTheme()),
-        home: const LoginBase()
-        ),
-      
-    );
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(textTheme: GoogleFonts.dmSansTextTheme()),
+      home: const LoginBase()
+      );
   }
 }

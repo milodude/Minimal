5594849018067813f68f3983cef4ee25a3542833
Matias Raverta <mraverta@nuba.com.ar>
2022-06-15 17:47:25 -0300

    12-Added dependency injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:coda_test/features/login/presentation/bloc/user/user_bloc.dart';
+import 'package:coda_test/features/login/presentation/pages/login_base.dart';
 import 'package:coda_test/features/login/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'injection_container.dart' as di;
+import 'injection_container.dart';
 
-void main() {
+void main() async {
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -12,14 +18,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(
-          textTheme:
-              GoogleFonts.dmSansTextTheme()),
-      home: const LoginPage(),
+    return BlocProvider(
+      create: (context) => sl<UserBloc>(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData().copyWith(textTheme: GoogleFonts.dmSansTextTheme()),
+        home: const LoginBase()
+        ),
+      
     );
   }
 }
-

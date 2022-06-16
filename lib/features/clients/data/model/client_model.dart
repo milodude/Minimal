0ddcc8f6967fe14a17f027303f2c6972fb0bc92e
Matias Raverta <mraverta@nuba.com.ar>
2022-06-16 10:43:09 -0300

    16-Added client model

---
+++ lib/features/clients/data/model/client_model.dart
@@ -0,0 +1,21 @@
+import 'package:coda_test/features/clients/domain/entities/client.dart';
+
+class ClientModel extends ClientData {
+  const ClientModel({
+    required super.id,
+    required super.firstName,
+    required super.lastName,
+    required super.email,
+  });
+
+  factory ClientModel.fromJson(Map<String, dynamic> client) {
+      print(client);
+
+    return ClientModel(
+      id: client['id'],
+      firstName: client['firstname'],
+      lastName: client['lastname'],
+      email: client['email'],
+    );
+  }
+}
---
+++ test/features/clients/data/model/client_model_test.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:coda_test/features/clients/data/model/client_model.dart';
+import 'package:coda_test/features/login/data/model/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  group('Client model tests: ', () {
+    var tUser = const ClientModel(
+      id: 816,
+      firstName: 'Franco',
+      lastName: 'Camiletti',
+      email: 'matias@agencycoda.com',
+    );
+
+    testWidgets('Should be a client model', (tester) async {
+      expect(tUser, isA<ClientModel>());
+    });
+    
+    testWidgets('Should parse a client from a json response', (tester) async {
+       //Arrange
+      final Map<String, dynamic> decoded =
+          json.decode(fixture('clients/client.json'));
+      //Act
+      var result = ClientModel.fromJson(decoded);
+      //Assert
+      expect(result, equals(tUser));
+    });
+  });
+}

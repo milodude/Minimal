f471b59fcfe0bc3d39d900db114da4bb66f9efbe
Matias Raverta <mraverta@nuba.com.ar>
2022-06-18 12:36:46 -0300

    21-Refactor on provider for main file. Deleted base login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
-import 'package:coda_test/features/login/presentation/pages/login_base.dart';
+import 'package:coda_test/features/clients/presentation/bloc/client/client_bloc.dart';
+import 'package:coda_test/features/clients/presentation/pages/clients_page.dart';
+import 'package:coda_test/features/login/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'features/login/presentation/bloc/user/user_bloc.dart';
 import 'injection_container.dart' as di;
+import 'injection_container.dart';
 
 void main() async {
   await di.init();
   runApp(const MyApp());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyApp();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +32,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(textTheme: GoogleFonts.dmSansTextTheme()),
-      home: const LoginBase()
-      );
+      initialRoute: LoginPage.routeName,
+      routes: {
+        '/': (context) => BlocProvider.value(value: sl<UserBloc>(),child: const LoginPage(),),
+        ClientsPage.routeName: (context) => BlocProvider.value(value: sl<ClientBloc>(),child: const ClientsPage(),),
+      },
+    );
   }
 }

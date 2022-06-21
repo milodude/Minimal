02f21e4974d466faa85528a8f4595f8d64f52a28
Matias Raverta <mraverta@nuba.com.ar>
2022-06-21 15:32:06 -0300

    69-Refactor on base modal for editing and adding.

--- lib/core/constants/text.dart
+++ lib/core/constants/text.dart
@@ -1,2 +1,10 @@
+//!Log in
 const String appName = 'minimal'; 
 const String logIn = 'LOG IN';
+
+//!Clients
+const String clientsTitle = 'CLIENTS';
+const String searchBarLabel = 'Search...';
+const String searchBarHintText = 'Client name..';
+const String buttonAddNewText = 'ADD NEW';
+const String buttonLoadMoreText = 'LOAD MORE';
--- lib/features/clients/presentation/pages/base_add_edit_client_modal.dart
+++ lib/features/clients/presentation/pages/base_add_edit_client_modal.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -52,161 +53,198 @@ class _BAddClientModalState extends State<BaseAddEditClientModal> {
       final imageTemporary = File(image.path);
       setState(() => this.image = imageTemporary);
     } on PlatformException catch (e) {
+      // ignore: avoid_print
       print('Failed to pick image: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final isKeyBoardOpen = MediaQuery.of(context).viewInsets.bottom != 0;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return SizedBox(
-      height: MediaQuery.of(context).size.height - 100,
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.title,
-                      style: const TextStyle(fontSize: 27),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 32),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () async => pickImage(),
-                      child: image != null
-                          ? CircleAvatar(
-                              backgroundImage: FileImage(image!),
-                              radius: 76,
-                            )
-                          : const CircleAvatar(
-                              backgroundImage: AssetImage(uploadImage),
-                              radius: 76,
-                            ),
-                    ),
-                  ],
-                ),
-              ),
-              Form(
-                key: _modalFormKey,
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                        width: 295,
-                        height: 100,
-                        child: TextFormField(
-                          controller: firstNameInputController,
-                          decoration: const InputDecoration(
-                            labelText: 'First Name*',
-                          ),
-                          validator: (String? name) {
-                            if (name == null || name.isEmpty) {
-                              return 'You must add a firstname';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                        width: 295,
-                        height: 100,
-                        child: TextFormField(
-                          controller: lastNameInputController,
-                          decoration: const InputDecoration(
-                            labelText: 'Last Name*',
-                          ),
-                          validator: (String? name) {
-                            if (name == null || name.isEmpty) {
-                              return 'You must add a lastname';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                        width: 295,
-                        height: 100,
-                        child: TextFormField(
-                          controller: mailInputController,
-                          decoration: const InputDecoration(
-                            labelText: 'Email address*',
-                          ),
-                          validator: (String? name) {
-                            if (name == null || name.isEmpty) {
-                              return 'You must add an email';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
+      height: screenHeight,
+      width: screenWidth,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _getTitle(),
+              ],
+            ),
+            SizedBox(
+              height: isKeyBoardOpen ? 0 : screenHeight - screenHeight * 0.94,
+            ),
+            _getPhoto(isKeyBoardOpen),
+            SizedBox(
+              height: screenHeight - screenHeight * 0.94,
+            ),
+            _getForm(),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const CancelButton(),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(35)),
-                      fixedSize: const Size(159, 40),
-                      textStyle: const TextStyle(
-                          fontSize: 14, fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      print('llega?');
-                      if (!_modalFormKey.currentState!.validate()) return;
-                      final clientBloc = context.read<ClientBloc>();
-                      ClientData newClient = ClientData(
-                        id: widget.client == null ? 0 : widget.client!.id,
-                        firstName: firstNameInputController.text,
-                        lastName: lastNameInputController.text,
-                        email: mailInputController.text,
-                      );
-                      if (widget.client != null) {
-                        clientBloc.add(EditClient(clientToEdit: newClient));
-                      } else {
-                        clientBloc.add(AddClient(clientToAdd: newClient));
-                      }
-                      clientBloc.add(const GetClients());
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text(
-                      'SAVE',
-                      style: TextStyle(letterSpacing: 0.5),
-                    ),
-                  ),
+                  _getCancelButton(context),
+                  _getSaveButton(context),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  ElevatedButton _getSaveButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35)),
+        fixedSize: const Size(159, 40),
+        textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+      ),
+      onPressed: () {
+        if (!_modalFormKey.currentState!.validate()) return;
+        final clientBloc = context.read<ClientBloc>();
+        ClientData newClient = ClientData(
+          id: widget.client == null ? 0 : widget.client!.id,
+          firstName: firstNameInputController.text,
+          lastName: lastNameInputController.text,
+          email: mailInputController.text,
+        );
+        if (widget.client != null) {
+          clientBloc.add(EditClient(clientToEdit: newClient));
+        } else {
+          clientBloc.add(AddClient(clientToAdd: newClient));
+        }
+        clientBloc.add(const GetClients());
+        Navigator.of(context).pop();
+      },
+      child: const Text(
+        'SAVE',
+        style: TextStyle(letterSpacing: 0.5),
+      ),
+    );
+  }
+
+  GestureDetector _getCancelButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+      child: const CancelButton(),
+    );
+  }
+
+  Form _getForm() {
+    return Form(
+      key: _modalFormKey,
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: 295,
+              height: 100,
+              child: TextFormField(
+                controller: firstNameInputController,
+                decoration: const InputDecoration(
+                  labelText: 'First Name*',
+                ),
+                validator: (String? name) {
+                  if (name == null || name.isEmpty) {
+                    return 'You must add a firstname';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: 295,
+              height: 100,
+              child: TextFormField(
+                controller: lastNameInputController,
+                decoration: const InputDecoration(
+                  labelText: 'Last Name*',
+                ),
+                validator: (String? lastName) {
+                  if (lastName == null || lastName.isEmpty) {
+                    return 'You must add a lastname';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: 295,
+              height: 100,
+              child: TextFormField(
+                controller: mailInputController,
+                decoration: const InputDecoration(
+                  labelText: 'Email address*',
+                ),
+                validator: (String? email) {
+                  if (email == null || email.isEmpty) {
+                    return 'You must add an email';
+                  }
+
+                  if(!EmailValidator.validate(email)){
+                    return 'Invalid email address';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Visibility _getPhoto(bool isKeyBoardOpen) {
+    return Visibility(
+      visible: !isKeyBoardOpen,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 32),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () async => pickImage(),
+              child: image != null
+                  ? CircleAvatar(
+                      backgroundImage: FileImage(image!),
+                      radius: 76,
+                    )
+                  : const CircleAvatar(
+                      backgroundImage: AssetImage(uploadImage),
+                      radius: 76,
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _getTitle() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 32),
+      child: Text(
+        widget.title,
+        style: const TextStyle(fontSize: 27),
       ),
     );
   }
